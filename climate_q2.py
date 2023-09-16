ebc371489eca1d22e8ccf1af6299e75077ce1bf1
Victor Mou <victor.mou@griffithuni.edu.au>
2023-09-16 22:27:32 +1000

    complete the code climate_q2.py to fetch data

--- climate_q2.py
+++ climate_q2.py
@@ -5,6 +5,33 @@ years = []
 co2 = []
 temp = []
 
+reviews_df = pd.read_csv(r"climate.csv")
+# print(type(reviews_dt))
+
+# text = reviews_df.head(10)
+# print(text)
+
+yeardatas = reviews_df.Year
+# print(yeardatas)
+
+for yeardata in yeardatas:
+    # print(yeardata)
+    years.append(yeardata)
+
+# print(years)
+
+co2datas = reviews_df.CO2
+
+for co2data in co2datas:
+    co2.append(co2data)
+
+tempdatas = reviews_df.Temperature  
+
+for tempdata in tempdatas:
+    temp.append(tempdata)
+
+# print(years,co2,temp)
+
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
